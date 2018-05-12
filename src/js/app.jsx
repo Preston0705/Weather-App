1c7e2546c23e23c5f7a34d3dae19afd57336552f
Preston Hill <Preston0705@gmail.com>
2018-05-11 22:17:10 -0700

    title

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -8,7 +8,7 @@ export default class App extends React.Component {
     return (
       <div className='container-fluid'>
         <div className='jumbotron mt-3 alert-primary'>
-          <h1 className='display-3 text-center'>Origin Weather Application</h1>
+          <h1 className='display-3 text-center'>Weather Application</h1>
           <br/>
           <p className='lead text-center'>Always know if you'll need an umbrella!</p>
         </div>
